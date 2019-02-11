6191f41b69f2f22529b00d298936199a10d6786a
David Lyons <dlyons@berkeley.edu>
2019-02-11 12:23:14 -0800

    Create conversion_efficiency.sh

---
+++ conversion_efficiency.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## approximate the bisulfite conversion of your bs-seq libs with the output of 
+## bismark 'CX report'
+## ( ChrPt should not be methylated )
+
+#sum total unconverted cytosines
+methylC=`perl -wnl -e '/ChrPt/ and print;' $1 | awk '{sum+=$4} END {print sum}'`
+
+#sum total converted cytosines
+unmethylC=`perl -wnl -e '/ChrPt/ and print;' $1 | awk '{sum+=$5} END {print sum}'`
+
+#under 1% is usable for me, prefer under 0.5%
+#output such that 1.0 is 100%
+conversion_efficieny=`echo $methylC/\($methylC+$unmethylC\)|bc -l`
+echo
+echo $methylC/\($methylC+$unmethylC\)|bc -l
+echo
+echo
